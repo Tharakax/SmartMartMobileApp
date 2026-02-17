744c93a759a76bce9d171fd1c1c8c94a07910d10
Tharaka Lakshan <148984070+nimeshika90@users.noreply.github.com>
2026-02-17 14:15:32 +0530

    done updo payment info

--- app/src/main/java/com/example/lankasmartmart/PaymentinfoActivity.kt
+++ app/src/main/java/com/example/lankasmartmart/PaymentinfoActivity.kt
@@ -11,10 +11,6 @@ class PaymentinfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.paymentinfo)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
--- app/src/main/java/com/example/lankasmartmart/PaymentmethodActivity.kt
+++ app/src/main/java/com/example/lankasmartmart/PaymentmethodActivity.kt
@@ -1,20 +1,11 @@
 package com.example.lankasmartmart
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class PaymentmethodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.paymentmethod)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_payment_method)
     }
 }
